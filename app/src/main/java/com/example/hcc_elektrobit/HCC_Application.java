63f8dbdd974c4b87cee5f29d22da3c0243461098
Goose-py <slama.yassine3@gmail.com>
2024-10-22 15:39:35 +0200

    Create an application class for the app

---
+++ app/src/main/java/com/example/hcc_elektrobit/HCC_Application.java
@@ -0,0 +1,22 @@
+package com.example.hcc_elektrobit;
+
+import android.app.Application;
+import android.content.Context;
+
+
+// Application class to provide to context to the file provider
+public class HCC_Application extends Application {
+
+    private static volatile HCC_Application INSTANCE;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        INSTANCE = this;
+    }
+
+    protected static Context getAppContext(){
+
+        return INSTANCE.getApplicationContext();
+    }
+}
