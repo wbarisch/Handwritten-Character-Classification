7b6ff62408a2f42994d4916713057acc6289a873
Goose-py <slama.yassine3@gmail.com>
2024-10-22 16:16:19 +0200

    Added some more methods for replacing context use in logic classes

--- app/src/main/java/com/example/hcc_elektrobit/JFileProvider.java
+++ app/src/main/java/com/example/hcc_elektrobit/JFileProvider.java
@@ -1,10 +1,12 @@
 package com.example.hcc_elektrobit;
 
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
 import androidx.core.content.FileProvider;
+import java.io.File;
 import java.io.IOException;
 
 public class JFileProvider extends FileProvider {
@@ -16,4 +18,25 @@ public class JFileProvider extends FileProvider {
         AssetManager assetManager = getFileProviderContext().getAssets();
         return assetManager.openFd(searchString);
     }
+
+    protected static File getCacheDir(){
+        return getFileProviderContext().getCacheDir();
+    }
+
+    protected static File getInternalDir(){
+        return getFileProviderContext().getFilesDir();
+    }
+
+    protected static File getExternalDir(String _path){
+        return getFileProviderContext().getExternalFilesDir(_path);
+    }
+
+    protected static ContentResolver getContentResolver(){
+        return getFileProviderContext().getContentResolver();
+    }
+
+    protected static AssetManager getAssets(){
+        return getFileProviderContext().getAssets();
+    }
+
 }
