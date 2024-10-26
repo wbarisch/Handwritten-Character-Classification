13e669aa0e5b65006ffdebe01231a9ef67dc5dcc
Joshua Oldridge <oldridgejoshua@gmail.com>
2024-10-26 16:15:26 +0200

    Deleted non-used method

--- app/src/main/java/com/example/hcc_elektrobit/JMainActivity.java
+++ app/src/main/java/com/example/hcc_elektrobit/JMainActivity.java
@@ -247,8 +247,4 @@ public class JMainActivity extends AppCompatActivity implements TimeoutActivity 
         return bitmap;
     }
 
-    public void enterTrainingMode() {
-        Log.d("JMainActivity", "Training mode enabled");
-    }
-
 }
