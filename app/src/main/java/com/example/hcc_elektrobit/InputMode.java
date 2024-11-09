24bde71813f47095577a891efc8063b8bd09e4ec
Chernet <chernetx@gmail.com>
2024-11-09 15:13:33 +0100

    Created the class InputMode.java.

---
+++ app/src/main/java/com/example/hcc_elektrobit/InputMode.java
@@ -0,0 +1,11 @@
+package com.example.hcc_elektrobit;
+
+public class InputMode {
+
+    // Constants to designate Input Modes
+    public static final int DEFAULT = 0; // All types: uppercase and lowercase letters, numbers, and symbols
+    public static final int UPPERCASE = 1; // Uppercase mode
+    public static final int LOWERCASE = 2; // Lower case mode
+    public static final int NUMBER = 3; // Number mode
+
+}
