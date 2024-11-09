788013b4bc869c9dbd280463099463a45f6667b5
Eugen Zdrincu <eugenzdrincu@gmail.com>
2024-11-09 15:10:05 +0200

    create test for history Item class

---
+++ app/src/androidTest/java/com/example/hcc_elektrobit/HistoryItemTest.java
@@ -0,0 +1,43 @@
+package com.example.hcc_elektrobit;
+
+import android.graphics.Bitmap;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HistoryItemTest {
+
+    private Bitmap testBitmap;
+    private String testPrediction;
+    private Object testTensor;
+    private HistoryItem historyItem;
+
+    @Before
+    public void setUp() {
+        testBitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
+        testPrediction = "TestPrediction";
+        testTensor = new float[][]{{1.0f, 2.0f}, {3.0f, 4.0f}};
+        historyItem = new HistoryItem(testBitmap, testPrediction, testTensor);
+    }
+
+    @Test
+    public void testGetBitmap() {
+        assertEquals(testBitmap, historyItem.getBitmap());
+    }
+
+    @Test
+    public void testGetPred() {
+        assertEquals(testPrediction, historyItem.getPred());
+    }
+
+    @Test
+    public void testGetOutputCollection() {
+        assertEquals(testTensor, historyItem.getOutputCollection());
+    }
+
+    @Test
+    public void testGetModel() {
+        assertNull(historyItem.getModel());
+    }
+}
