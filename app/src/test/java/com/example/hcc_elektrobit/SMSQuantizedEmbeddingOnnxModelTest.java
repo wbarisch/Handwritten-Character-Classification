df22a75f1c3ad6b527f0cd27721fc857c8dd3c0f
Eugen Zdrincu <eugenzdrincu@gmail.com>
2024-11-12 13:41:07 +0200

    test for history item and quantized model

--- app/src/test/java/com/example/hcc_elektrobit/SMSQuantizedEmbeddingOnnxModelTest.java
+++ app/src/test/java/com/example/hcc_elektrobit/SMSQuantizedEmbeddingOnnxModelTest.java
@@ -1,4 +1,31 @@
 package com.example.hcc_elektrobit;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
 public class SMSQuantizedEmbeddingOnnxModelTest {
+
+    SMSQuantizedEmbeddingOnnxModel model = SMSQuantizedEmbeddingOnnxModel.getInstance();
+
+    @Test
+    public void testEmbedBitmap() {
+        try {
+            model.embedBitmap(null);
+        } catch (Exception e){
+            fail();
+        }
+    }
+
+    @Test
+    public void testPreprocessBitmap() {
+        try {
+            model.preprocessBitmap(null);
+        } catch (Exception e){
+            fail();
+        }
+    }
 }
