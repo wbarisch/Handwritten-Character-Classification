1de6ea7a357d532f886c619579c0c80a891b9bc1
osama <os.os20.osama@gmail.com>
2024-11-27 13:56:23 +0100

    Fix typo

--- app/src/main/java/com/example/hcc_elektrobit/driving_mode/DrivingMode.java
+++ app/src/main/java/com/example/hcc_elektrobit/driving_mode/DrivingMode.java
@@ -12,6 +12,7 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
